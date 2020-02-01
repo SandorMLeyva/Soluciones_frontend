e7071d5b1f358b0ecf609deedece656543635e80
Sandor Martin Leyva <sandormartin37@gmail.com>
2020-02-01 16:36:20 -0500

    Added yes or not component

---
+++ src/components/YesOrNot/index.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    '& > *': {
+      margin: theme.spacing(1),
+    },
+  },
+}));
+
+export default function ContainedButtons(props) {
+  const classes = useStyles();
+  const {onYesClick, onNoClick} = props;
+
+  return (
+    <div className={classes.root} style={{textAlign:"center"}}>
+      <Button variant="contained" color="primary" onClick={onYesClick}>
+        Si
+      </Button>
+      <Button variant="contained" color="secondary" onClick={onNoClick}>
+        No
+      </Button>
+    </div>
+  );
+}
