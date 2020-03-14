b740c8368af4f238ca338b0e63dd23a57bcb9595
Sandor Martin Leyva <sandormartin37@gmail.com>
2020-03-14 03:14:36 -0400

    Added COOLORS to helpers

--- src/helpers.js
+++ src/helpers.js
@@ -1,3 +1,12 @@
 export function getRndInteger(min, max) {
     return Math.floor(Math.random() * (max - min)) + min;
 }
+
+export const COOLORS = {
+    'UPEN': '#EA4335', 
+    'APEN': '#4285F4',
+    'PROC': '#FBBC05',
+    'FIN': '#34A853',
+    'WARR': '#66757F',
+    'NO_WARR': '#FFFFFF'
+};
